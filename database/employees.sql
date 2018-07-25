38ef17a94397e609a0796bbb868c887fa9a534a6
jaethao <mjathao@gmail.com>
2018-07-25 13:38:04 -0500

    config to connect to database and created html

---
+++ database/employees.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE IF NOT EXISTS employees (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  address varchar(255) NOT NULL,
+  salary int(10) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+INSERT INTO employees (id, name, address, salary) VALUES
+(1, 'Roland Mendel', '67 Araquil, Madrid', 5000),
+(2, 'Victoria Ashworth', '35 King George, London', 6500),
+(3, 'Martin Blank', '25 Rue Lauriston, Paris', 8000);
